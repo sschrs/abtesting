7d4ea016e927128711acb2ff78af2be5f0e455b0
sschrs <s_ozarslan07@outlook.com>
2022-10-16 13:00:29 +0300

    Sample and Population opitons are added for Std and Variance functions

--- utils/stats.go
+++ utils/stats.go
@@ -15,18 +15,28 @@ func Mean(data []float64) float64 {
 }
 
 // Variance returns the variance of a []float array
-func Variance(data []float64) float64 {
+func Variance(data []float64, sample ...bool) float64 {
+	var minus int
+	if len(sample) <= 0 {
+		minus = 1
+	} else {
+		if sample[0] == false {
+			minus = 0
+		} else {
+			minus = 1
+		}
+	}
 	mean := Mean(data)
 	sum := 0.0
 	for _, i := range data {
 		sum += math.Pow(i-mean, 2)
 	}
-	return sum / float64(len(data)-1)
+	return sum / float64(len(data)-minus)
 }
 
 // Std returns the standard deviation of a []float array
-func Std(data []float64) float64 {
-	return math.Sqrt(Variance(data))
+func Std(data []float64, sample ...bool) float64 {
+	return math.Sqrt(Variance(data, sample...))
 }
 
 // Max returns the maximum value of an array
